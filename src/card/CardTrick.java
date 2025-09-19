9b3c560e68f19e237f2894691707847a5157458e
kay843 <Kainatimran511@gmail.com>
2025-09-18 20:14:54 -0400

    master version 2

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * @author Kainat Imran - 991721311
  * 
  */
 public class CardTrick {
